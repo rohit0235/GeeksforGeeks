e5effabc74f601765c7a17751480c68e9bd6ced0
ROHIT KUMAR SAHU <rohitkumarsahu2022@vitbhopal.ac.in>
2024-09-20 10:01:33 +0530

    Create Facing the sun.cpp

---
+++ Facing the sun.cpp
@@ -0,0 +1,12 @@
+int countBuildings(vector<int> &height) {
+        // code here
+        int n=height.size();
+        int maxi=height[0];
+        int count=1;
+        for (int i=1;i<n;i++){
+                    maxi=max(maxi,height[i-1]);
+                    if (height[i]>maxi){
+                        count++;
+                    }
+        }
+        return count;

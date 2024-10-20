5c5964402da3cd038e543f249b7959ca1921b058
ROHIT KUMAR SAHU <rohitkumarsahu2022@vitbhopal.ac.in>
2024-10-21 01:44:40 +0530

    Update Kth Smallest Factor.cpp

--- Kth Smallest Factor.cpp
+++ Kth Smallest Factor.cpp
@@ -1,12 +1,11 @@
 class Solution {
   public:
     int kThSmallestFactor(int n, int k) {
-            int s=k;
-            for (int i=1;i<=n;i++){
+           for (int i=1;i<=n;i++){
                if (n%i==0){
-                    s-=1;
+                    k-=1;
                 } 
-                if (s==0){
+                if (k==0){
                     return i;
                 }
                 

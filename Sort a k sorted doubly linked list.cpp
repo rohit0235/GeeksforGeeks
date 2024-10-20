aa9e504f6c6c2d07fd5ee18a97b9c63ed70366e9
ROHIT KUMAR SAHU <rohitkumarsahu2022@vitbhopal.ac.in>
2024-10-20 21:09:11 +0530

    Create Sort a k sorted doubly linked list.cpp

---
+++ Sort a k sorted doubly linked list.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+    // function to sort a k sorted doubly linked list
+   DLLNode *sortAKSortedDLL(DLLNode *head, int k)
+
+{
+        priority_queue < int, vector<int>, greater<int> > pq;
+        
+        DLLNode *temp=head;
+        DLLNode *put =head;
+        
+        while(temp!=NULL)
+        {
+            pq.push(temp->data);
+            if(pq.size()>k)
+            {
+                put->data=pq.top();
+                pq.pop();
+                put=put->next;
+            }
+            temp=temp->next;
+        }
+        
+        while(!pq.empty())
+        {
+            put->data=pq.top();
+            pq.pop();
+            put=put->next;
+        }
+        
+        return head;
+    }
+
+
+};

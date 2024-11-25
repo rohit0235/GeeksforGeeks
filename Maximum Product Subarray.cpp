b69e881ea133762975159858d2ab8a5acb0985bc
ROHIT KUMAR SAHU <rohitkumarsahu2022@vitbhopal.ac.in>
2024-11-25 21:14:18 +0530

    Create Maximum Product Subarray.cpp

---
+++ Maximum Product Subarray.cpp
@@ -0,0 +1,16 @@
+            int maxi=arr[0];
+            int n=arr.size();
+            int leftsum=1;
+            int  rightsum=1;
+            for (int i=0;i<n;i++){
+                    leftsum*=arr[i];
+                    rightsum*=arr[n-i-1];
+                 maxi=max(maxi,max(leftsum,rightsum));
+                if (leftsum==0)   leftsum=1;
+                if (rightsum==0)   rightsum=1;
+                
+               
+            }
+            return maxi;
+            
+        

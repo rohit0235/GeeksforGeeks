e6793417d36396b82706c84eec5fe107cd2f5374
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-01-07 22:11:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/House Robber/house-robber.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int maximizeMoney(int N , int K) {
+          if (N%2==0) return K*(N/2);
+          else return K*(N/2+1);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,K;
+
+        cin>>N>>K;
+
+        Solution ob;
+        cout << ob.maximizeMoney(N,K) << endl;
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+// } Driver Code Ends

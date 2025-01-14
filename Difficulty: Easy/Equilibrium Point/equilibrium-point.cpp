4e4def0a79b034dd6c747e6c75948e9ee243c081
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-01-14 09:39:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Equilibrium Point/equilibrium-point.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find equilibrium point in the array.
+    int findEquilibrium(vector<int> &arr) {
+          
+          int total=accumulate(arr.begin(),arr.end(),0);
+          int leftsum=0;
+          int n=arr.size();
+          for (int i=0;i<n;i++){
+              int rightsum=total-leftsum-arr[i];
+              if (leftsum==rightsum){
+                  return i;
+              }
+              leftsum+=arr[i];
+          }
+          
+          return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To discard any leftover newline characters
+    while (t--)   // while testcases exist
+    {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.findEquilibrium(arr) << endl;
+        cout << "~" << endl;
+    }
+}
+// } Driver Code Ends

b6a71dddc238d31ea55ff33da89e4466ce6dc90c
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-02-07 19:35:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smaller on Left/smaller-on-left.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> Smallestonleft(int arr[], int n);
+
+int main() {
+
+    int t;
+
+    cin >> t;
+
+    while (t--) {
+
+        int n;
+        cin >> n;
+        int arr[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+
+        vector<int> b;
+        b = Smallestonleft(arr, n);
+        for (int i = 0; i < n; i++)
+            cout << b[i] << " ";
+
+        cout << endl;
+    
+cout << "~" << "\n";
+}
+
+    return 0;
+}
+// } Driver Code Ends
+
+
+vector<int> Smallestonleft(int arr[], int n) {
+        
+        vector<int>ans(n,-1);
+        
+        set<int>s;
+        s.insert(arr[0]);
+        for (int i=1;i<n;i++){ 
+            
+            auto it=s.lower_bound(arr[i]);
+            if (it!=s.begin()) {
+                 it--;
+                  ans[i]=*it;
+            }
+            s.insert(arr[i]);
+        }
+        
+        return ans;
+        
+        
+}

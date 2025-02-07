4a8ee8f952a30493e8f4d643399c5600ef447599
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-02-07 11:49:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Array/sum-of-array.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    // Function to return sum of elements
+    int arraySum(vector<int>& arr) {
+        
+        int sum=accumulate(arr.begin(),arr.end(),0);
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    while (t--) {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        Solution ob;
+        int ans = ob.arraySum(nums); // Pass the vector nums to the sum function
+        cout << ans << "\n~\n";
+    }
+    return 0;
+}
+// } Driver Code Ends

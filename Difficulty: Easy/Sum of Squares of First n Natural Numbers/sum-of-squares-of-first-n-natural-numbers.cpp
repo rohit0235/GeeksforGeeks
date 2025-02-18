789b9f158be0fe4202f100f883e4c2dc29b175cc
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-02-19 01:31:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum of Squares of First n Natural Numbers/sum-of-squares-of-first-n-natural-numbers.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int sumOfSquares(int n) {
+        
+        return ((n)*(n+1)*(2*n+1))/6;
+            
+            
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int number;
+        cin >> number;
+
+        Solution ob;
+        int ans = ob.sumOfSquares(number);
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends

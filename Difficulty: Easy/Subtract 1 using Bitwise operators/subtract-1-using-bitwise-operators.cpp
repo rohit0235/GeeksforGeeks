df9e99505b97c3fab7021d16a48a37ccf4dc9010
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-02-19 01:41:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Subtract 1 using Bitwise operators/subtract-1-using-bitwise-operators.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int subtractOne(int x) {
+        // code here
+        
+        return x + (~0); 
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int number;
+        cin >> number;
+        Solution obj;
+        int ans = obj.subtractOne(number);
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends

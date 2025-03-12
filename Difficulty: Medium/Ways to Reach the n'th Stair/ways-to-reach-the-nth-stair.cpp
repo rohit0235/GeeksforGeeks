cc4931e9fe93707e9d892389991983101070ab8e
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-03-12 19:30:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.cpp
+++ Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.cpp
@@ -7,17 +7,37 @@ using namespace std;
 
 class Solution {
   public:
+  
+  
+     int solve(int i,int n,vector<int>&dp){
+              if (i == n) return 1; 
+           if (i>n) return 0;
+            
+           if(dp[i]!=-1) return dp[i];
+           
+           return dp[i]=(solve(i+1,n,dp)+solve(i+2,n,dp));
+           
+     }
+    
     int countWays(int n) {
+        
+        vector<int>dp(n,-1);
+        return solve(0,n,dp);
+        
+        
         // your code here
-               int prev1 = 1;
-        if(n == 1) return 1;
-        int prev2 = 2;
-        for(int i = 3; i <= n; i++) {
-            int next = prev1 + prev2;
-            prev1 = prev2;
-            prev2 = next;
-        }
-        return prev2;
+        //       int prev1 = 1;
+        // if(n == 1) return 1;
+        // int prev2 = 2;
+        // for(int i = 3; i <= n; i++) {
+        //     int next = prev1 + prev2;
+        //     prev1 = prev2;
+        //     prev2 = next;
+        // }
+        // return prev2;
+        
+        
+        
     }
 };
 

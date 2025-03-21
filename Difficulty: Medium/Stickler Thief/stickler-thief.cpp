7ea199c855bd5dd35a805a79118713eaa7e8cec4
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-03-21 09:28:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stickler Thief/stickler-thief.cpp
+++ Difficulty: Medium/Stickler Thief/stickler-thief.cpp
@@ -9,21 +9,33 @@ typedef long long int ll;
 class Solution {
   public:
     int n;
-    
-    int solve(vector<int>&arr,int i,vector<int>&dp){
+    // using memorization
+    // int solve(vector<int>&arr,int i,vector<int>&dp){
           
-          if (i>=n) return 0;
-          if (dp[i]!=-1) return dp[i];
-          int chori=arr[i]+solve(arr,i+2,dp);
-          int chorinahi=solve(arr,i+1,dp);
+    //       if (i>=n) return 0;
+    //       if (dp[i]!=-1) return dp[i];
+    //       int chori=arr[i]+solve(arr,i+2,dp);
+    //       int chorinahi=solve(arr,i+1,dp);
           
-          return  dp[i]=max(chori,chorinahi);
-    }
+    //       return  dp[i]=max(chori,chorinahi);
+    // }
     int findMaxSum(vector<int>& arr) {
         // code here
           n=arr.size();
-          vector<int>dp(n+1,-1);
-          return solve(arr,0,dp);
+          vector<int>dp(n+1,0);
+          dp[0]=arr[0];
+          dp[1]=max(arr[0],arr[1]);
+          
+          
+          for(int i=2;i<n;i++){
+                
+                dp[i]= max(dp[i-2]+arr[i],dp[i-1]);
+          }
+          
+           return dp[n-1];
+        //   return solve(arr,0,dp);
+            
+        
     }
 };
 

43dc0038ee63f7c2a8a9632fe83ef68fbcaf69ae
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-03-24 11:28:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
@@ -4,34 +4,35 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 // User function Template for C++
 
 class Solution {
   public:
- int solve(vector<int> &arr, int i, int j, vector<vector<int>> &dp) {
-    if (i == j) return 0;
-    if (dp[i][j] != -1) return dp[i][j];
-
-    long long res = LLONG_MAX; // To handle large values
-
-    for (int k = i; k < j; k++) {
-        long long left = solve(arr, i, k, dp);
-        long long right = solve(arr, k + 1, j, dp);
-        long long cost = left + right + 1LL * arr[i - 1] * arr[k] * arr[j]; // 1LL for safe multiplication
-
-        res = min(res, cost);
+  
+    int solve (vector<int> &arr,int i,int j,   vector<vector<int>>&dp){
+           
+           if (i==j) return 0;
+           if (dp[i][j]!=-1) return dp[i][j];
+           int res=INT_MAX;
+           for (int k=i;k<j;k++){
+                  
+              int cost =solve (arr,i,k,dp)+solve (arr,k+1,j,dp)+arr[i-1]*arr[k]*arr[j];
+              res=min(res,cost);
+           }
+           
+            dp[i][j]=res;
+            return dp[i][j];
     }
-
-    dp[i][j] = res;
-    return dp[i][j];
-}
-
-int matrixMultiplication(vector<int> &arr) {
-    int n = arr.size();
-    vector<vector<int>> dp(n, vector<int>(n, -1));
-    return solve(arr, 1, n - 1, dp);
-}
+    int matrixMultiplication(vector<int> &arr) {
+        int n =arr.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        return solve (arr,1,n-1,dp);
+        
+    }
 };
+
+
 
 //{ Driver Code Starts.
 

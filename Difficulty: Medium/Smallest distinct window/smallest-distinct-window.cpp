edd7480e374951c84e146bcc1b2901543527093e
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-05-04 13:48:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Smallest distinct window/smallest-distinct-window.cpp
+++ Difficulty: Medium/Smallest distinct window/smallest-distinct-window.cpp
@@ -4,69 +4,44 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
-    int findSubString(string& s) {
-       
-             vector<int>count(256,0); 
-        int len=s.size(),first=0,second=0; 
-        int diff=0;
-        
-        
-        
-        // Counting Unique Character in whole string in diff
-        while(first<s.size())
-        {
-          if(count[s[first]]==0)
-              diff++;
-              
-              count[s[first]]++; 
-              first++; 
-          
-        };
-        // clearing count after cal diff
-        for(int i=0; i<256;i++)
-        count[i]=0; 
-        
-        first=0; 
-        
-        while (second<s.size())
-        {
-            // second will move ahead till diff exist
-            
-            while (diff&&second<s.size())
-            {
-                if(count[s[second]]==0)
-                diff--; 
-                
-                count[s[second]]++;
-                second++;
+    int findSubString(string& str) {
+        // code here
+        unordered_map<char,int>freq;
+        unordered_map<char,int>result;
+        for (char c:str) {
+           freq[c]=1;
+        }
+        int char_=freq.size();    
+        freq.clear();
+        int beg=0;
+        int min_=INT_MAX;
+        int cnt=0;
+        for(int end= 0; end<str.size();++end) {
+            char cur =str[end];
+            result[cur]++;
+                if(result[cur]==1)
+                cnt++;
+            while(cnt ==char_) {
+                if(end -beg+1< min_) 
+                    min_=end -beg + 1;
+                char new_=str[beg];
+                result[new_]--;
+                if(result[new_]==0) 
+                    cnt--;
+                beg++;
             }
-            
-            len=min(len,second-first); 
-            
-            // first will move ahead once diff becomes 0
-            while (diff!=1)
-            {
-                len=min(len,second-first); 
-                count[s[first]]--;
-
-                if(count[s[first]]==0)      
-                diff++;
-                
-                
-                first++; 
-            }
-            
         }
         
-        return len; 
-                  
+        return min_;
     }
 };
 
+
 //{ Driver Code Starts.
-//   Driver code
+//      Driver code
 int main() {
     int t;
     cin >> t;

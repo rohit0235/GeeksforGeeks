d7f1a715947f89352969fe7b23bd8064ba49374d
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-06-18 00:48:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Disjoint set (Union-Find)/disjoint-set-union-find.cpp
@@ -0,0 +1,25 @@
+/*Complete the functions below*/
+int find(int par[], int x) {
+    // add code here
+    
+    if (par[x]==x) return x;
+    
+    return find(par,par[x]);
+    
+    
+}
+
+void unionSet(int par[], int x, int z) {
+    // add code here.
+    
+    
+     int lrep = find(par,x);
+     int jrep = find(par,z);
+    //  int jrep = par[z];
+     
+     par[lrep]=jrep;
+     
+     return ;
+    
+    
+}

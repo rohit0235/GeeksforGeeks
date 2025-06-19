ae8616b72a8d8bc30335c281e5a6b1513b302943
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-06-19 15:59:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count BST nodes that lie in a given range/count-bst-nodes-that-lie-in-a-given-range.cpp
@@ -0,0 +1,32 @@
+/*
+// Tree Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    // Constructor to initialize a new node
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int count =0;
+    int getCount(Node *root, int l, int h) {
+                
+                if (root==NULL) return 0;
+                  if (root->data<=h && root->data>=l) count++;
+                 getCount(root->left , l ,h);           
+                 getCount(root->right , l ,h);           
+                
+                return count;
+                
+        
+    }
+};

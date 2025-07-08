1725662e5386848c29d45f2bf13fe6f1b8a03d26
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-07-08 23:56:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.cpp
+++ Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.cpp
@@ -1,73 +1,37 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
+  
+  // 10 5 2 7 1 -10 
+  
+  // 10 15 - 2
+  // 5 2 7 1 - 4
+  // 
     int longestSubarray(vector<int>& arr, int k) {
-       
-       // 10 15 17 24 25 15
-       
-       // -5 3 -11 -9 -5 7
-       // 10 0 20 50 
-       
-       // 
-       unordered_map<int,int>mp;
-       int maxlength=0;
-       int n=arr.size();
-       int sum=0;
-       for (int i=0;i<n;i++){
-           sum+=arr[i];
-           
-          if (sum==k){
-            //   cout<<i<<" ";
-              maxlength=max(maxlength,i+1);
-          }
-         else if (mp.find(sum-k)!=mp.end()){
-                maxlength=max(maxlength,i-mp[sum-k]);
-          }
-           
-           if (mp.find(sum)==mp.end()){
-               mp[sum]=i;
-           }
-           
-       }
-       
-       return maxlength;
-       
+                 // 10 15 17 24 25 
+                int n =arr.size();
+                int maxi =0;
+                int sum =0;
+                unordered_map<int,int>mp;
+                for(int r=0;r<n;r++){
+                   sum+=arr[r];
+                    if (sum==k){
+                         maxi = r+1;
+                    }
+                    if (mp.find(sum-k)!=mp.end()){ 
+                             maxi =max(maxi  ,  r-mp[sum-k])      ;
+                    }
+                     if (mp.find(sum)==mp.end()) mp[sum]=r;
+                       
+                }
+                
+                
+                    if (sum==k){
+                         maxi = n;
+                    }
+                
+                return maxi;
+                 
+                 
+        
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // Ignore newline character after t
-
-    while (t--) {
-        vector<int> arr;
-        int k;
-        string inputLine;
-
-        getline(cin, inputLine); // Read the array input as a line
-        stringstream ss(inputLine);
-        int value;
-        while (ss >> value) {
-            arr.push_back(value);
-        }
-
-        cin >> k;
-        cin.ignore(); // Ignore newline character after k input
-
-        Solution solution;
-        cout << solution.longestSubarray(arr, k) << "\n";
-        cout << "~\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends

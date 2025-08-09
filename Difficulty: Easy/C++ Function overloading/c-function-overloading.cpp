2454ad7199c95a942d9796b3dd6550e2594682ca
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-08-09 16:15:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/C++ Function overloading/c-function-overloading.cpp
@@ -0,0 +1,13 @@
+void volume(int s) {
+      cout<< s*s*s<<endl;
+}
+
+void volume(int r, int h) {
+    // Add your code here.
+    cout<< 3.14159*r*r*h<<endl;
+}
+
+void volume(int l, int b, int h) {
+    // Add your code here.
+    cout<< l*b*h<<endl;
+}

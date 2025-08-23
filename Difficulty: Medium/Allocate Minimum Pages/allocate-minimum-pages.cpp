eafae4cdebcf623326e77045e5c8fa44cd90d90a
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-08-23 11:29:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
@@ -1,63 +1,37 @@
 class Solution {
   public:
-  
-  
-    bool pos (int mid , vector<int>&arr , int k ){
-         
-           
-             int n =arr.size();
-             
-             int cnt =1;
-             
-             int pagesum=0;
-             
-             for(int i:arr){
-                  
-                    if (pagesum+i>mid){
-                          pagesum=i;
-                          cnt++;
-                    }else{
-                             pagesum+=i;
-                    }
-                    
-             }
-             
-             return cnt<=k;
-           
-    }
+  bool isPossible(vector<int> &arr, int k,int mid){
+      int sum=0,student=1;
+      for(int x:arr){
+          sum+=x;
+          if(sum>mid){
+              student++;
+              sum=x;
+          }
+          
+      }
+      return student<=k;
+  }
     int findPages(vector<int> &arr, int k) {
         // code here
-              
-              int n =arr.size();
-              
-              
-              if (k>n)  return -1;
-              
-              int l= *max_element(arr.begin(),arr.end());
-              int r =accumulate(arr.begin(),arr.end(),0);
-              
-              
-              int ans =-1;
-                 
-                 
-              while (l<=r){
-                    
-                    int mid = l+(r-l)/2;
-                    
-                    
-                    if (pos(mid , arr, k)){
-                            
-                            ans =mid ;
-                            r = mid -1;
-                        
-                    }
-                    
-                    else  l = mid+1;
-                     
-              }
-              
-              return ans ;
-              
-              
+        if(k>arr.size()) return -1;
+        int sum=0,mx=INT_MIN;
+        for(int x:arr){
+            sum+=x;
+            mx=max(mx,x);
+        }
+        int low=mx,high=sum,mid,ans=INT_MAX;
+        while(low<=high){
+            mid=(low+high)/2;
+            if(isPossible(arr,k,mid)){
+                ans=min(ans,mid);
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
     }
 };
+

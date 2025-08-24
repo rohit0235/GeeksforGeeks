a420750d76fd9e66e457bbefc92107d71b6b81e7
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-08-24 12:42:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Identical Trees/identical-trees.cpp
+++ Difficulty: Easy/Identical Trees/identical-trees.cpp
@@ -1,35 +1,29 @@
-/* A binary tree node
-
-
-struct Node
-{
+/*
+class Node {
+public:
     int data;
-    struct Node* left;
-    struct Node* right;
+    Node* left;
+    Node* right;
 
-    Node(int x){
+    Node(int x) {
         data = x;
-        left = right = NULL;
+        left = nullptr;
+        right = nullptr;
     }
 };
 */
 
 class Solution {
   public:
-    // Function to check if two trees are identical.
-    bool isIdentical(Node *r1, Node *r2) {
-        // Your Code here
-             
-             if (r1==NULL && r2==NULL) return true;
-             if (r1==NULL || r2==NULL) return false;
-             
-             if (r1->data!=r2->data) return false;
-         
-             
-             
-             return (isIdentical(r1->left,r2->left) ,isIdentical(r1->right,r2->right));
-
-             
-             
+    bool isIdentical(Node* root1, Node* root2) {
+        // code here
+            if (root1==NULL && root2==NULL) return true;
+            if (root1==NULL || root2==NULL) return false;
+            
+            
+            if (root1->data!=root2->data) return false;
+            
+            
+            return isIdentical(root1->left,root2->left) && isIdentical(root1->right, root2->right);
     }
 };

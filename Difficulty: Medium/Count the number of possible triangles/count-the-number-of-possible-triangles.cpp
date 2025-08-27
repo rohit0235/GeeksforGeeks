eb6b351b40e077fba56f527245434e970eb047bb
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-08-27 10:55:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.cpp
+++ Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.cpp
@@ -1,64 +1,54 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    // Function to count the number of possible triangles.
     int countTriangles(vector<int>& arr) {
-       int n =arr.size();
-       sort (arr.begin(),arr.end());
-       int count=0;
-       for (int i=0;i<n;i++){
-            
-                 for (int j=i+1;j<n-1;j++){
-                     
-                     
-                  
+           
+             int n =arr.size();
+             
+             // 4 10  13 20
+             // 3 4 6 7
+             
+             // will do in n^2logn
+             
+             sort(arr.begin(),arr.end());
+             int count =0;
+             for(int i=0;i<n;i++){
+                   for(int j=i+1;j<n;j++){
+                       
+                        //   for(int k=j+1;k<n;k++){
+                               
+                                
+                        //         if (arr[i]+arr[j]>arr[k] && arr[j]+arr[k]>arr[i] && arr[k]+arr[i]>arr[j]){
+                                     
+                        //               count++;
+                        //         }
+                                 
+                        //   }
                          
-                         int sum=arr[i]+arr[j];
                          
-                         int k=j+1;
-                         
-                         while (k<n &&  sum>arr[k]){
-                             count++;
-                             k++;
-                         }
-                      
-                 }
-            
-       }
-       
-       return  count;
+                            int l= j+1;
+                            int r= n-1;
+                            int sum = arr[i]+arr[j];
+                            int ans = j;
+                            while (l<=r){
+                                  
+                                  int mid = l+(r-l)/2;
+                                //   cout<<arr[mid]<<" "<<sum<<" ";
+                                  if (arr[mid]>=sum){
+                                       
+                                         r= mid-1;
+                                  }else{  
+                                         ans = mid;
+                                         l = mid+1;
+                                  }
+                                   
+                            }
+                       
+                            count+=(ans-j);
+                            // cout<<ans<<endl;
+                   }
+             }
+             
+             return count;
+        
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // To ignore the newline after the integer input
-    while (t--) {
-        int n;
-        vector<int> a;
-        string input;
-
-        // Input format: first number n followed by the array elements
-        getline(cin, input);
-        stringstream ss(input);
-        int num;
-        while (ss >> num)
-            a.push_back(num);
-
-        Solution obj;
-        cout << obj.countTriangles(a) << "\n~\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends

b736141aa2d836247bca6434cd15310a7e12eb53
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-09-09 01:50:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Assign Mice Holes/assign-mice-holes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int assignHole(vector<int>& mices, vector<int>& holes) {
+        // code here
+         sort(mices.begin(),mices.end());
+         sort(holes.begin(),holes.end());
+         
+          
+          int maxi = 0;
+          for(int i=0;i<mices.size();i++){
+               maxi = max(maxi , abs(holes[i]-mices[i]));
+          }
+          
+          return maxi;
+        
+    }
+};

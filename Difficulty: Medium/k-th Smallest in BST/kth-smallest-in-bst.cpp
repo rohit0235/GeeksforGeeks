1fdbab2f4b93bbd55f78a720434aee0a3c71e108
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-10-15 18:36:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/k-th Smallest in BST/kth-smallest-in-bst.cpp
+++ Difficulty: Medium/k-th Smallest in BST/kth-smallest-in-bst.cpp
@@ -1,91 +1,9 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-#define MAX_HEIGHT 100000
-
-// Tree Node
-struct Node {
+/*
+class Node {
+  public:
     int data;
     Node* left;
     Node* right;
-
-    Node(int val) {
-        data = val;
-        left = right = NULL;
-    }
-};
-
-// Function to Build Tree
-Node* buildTree(string str) {
-    // Corner Case
-    if (str.length() == 0 || str[0] == 'N')
-        return NULL;
-
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for (string str; iss >> str;)
-        ip.push_back(str);
-
-    // Create the root of the tree
-    Node* root = new Node(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node*> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while (!queue.empty() && i < ip.size()) {
-
-        // Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-
-        // Get the current node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if (currVal != "N") {
-
-            // Create the left child for the current node
-            currNode->left = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if (i >= ip.size())
-            break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if (currVal != "N") {
-
-            // Create the right child for the current node
-            currNode->right = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-
-    return root;
-}
-
-
-// } Driver Code Ends
-/*Complete the function below
-
-struct Node {
-    int data;
-    Node *left;
-    Node *right;
 
     Node(int val) {
         data = val;
@@ -96,57 +14,20 @@ struct Node {
 
 class Solution {
   public:
-    // Return the Kth smallest element in the given BST
+   void dfs(Node* node , int &ans ,int &curr , int k){
+       if(node == NULL)return;
+       dfs(node->left  ,ans , curr ,k);
+       curr++;
+       if(curr == k)ans = node->data;
+       dfs(node->right , ans , curr , k);
+       
+   }
     int kthSmallest(Node *root, int k) {
-              if (root==NULL) return -1;
-          
-              vector<int>dat;
-              queue<Node*>q;
-              q.push(root);
-              
-              while (!q.empty()){
-                   
-                      int n=q.size();
-                      
-                      for (int i=0;i<n;i++){
-                          Node *fron=q.front();
-                           dat.push_back(fron->data);
-                           q.pop();
-                           if (fron->left) q.push(fron->left);
-                           if (fron->right) q.push(fron->right);
-                           
-                      }
-              }
-              
-              sort(dat.begin(),dat.end());
-               
-               if (k>dat.size()) return -1;
-              return dat[k-1];
+        // code here
+        int ans = -1,curr = 0;
+        dfs(root , ans , curr , k);
+        return ans;
+        
     }
 };
 
-//{ Driver Code Starts.
-int main() {
-
-    int t;
-    string tc;
-    getline(cin, tc);
-    t = stoi(tc);
-    while (t--) {
-        string s;
-        getline(cin, s);
-        Node* root = buildTree(s);
-
-        getline(cin, s);
-        int k = stoi(s);
-        //  getline(cin, s);
-        Solution obj;
-        cout << obj.kthSmallest(root, k) << endl;
-        // cout<<"~"<<endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends

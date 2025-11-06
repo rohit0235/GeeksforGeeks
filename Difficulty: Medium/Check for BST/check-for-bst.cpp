c42fbd00ea0296f484e05f46f381e452ea49bbb9
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-11-06 21:08:31 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Check for BST/check-for-bst.cpp
@@ -0,0 +1,37 @@
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+  
+   bool solve(Node *root, int mini, int maxi){
+         
+          if (root==NULL) return true;
+         
+          if (root->data<=mini || root->data>=maxi){
+                return false;
+          }   
+
+          
+        //   if (root->left->data)
+          return solve(root->left,mini,root->data) && solve(root->right,root->data, maxi);
+   }
+  
+    bool isBST(Node* root) {
+        // code here
+         if (root==NULL) return true;
+         
+         return solve(root, INT_MIN, INT_MAX);
+    }
+};

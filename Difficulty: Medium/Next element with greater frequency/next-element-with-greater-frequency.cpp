76dabd869346f14b8ff540f4eb181a20c17e9fb9
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2026-01-18 10:14:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next element with greater frequency/next-element-with-greater-frequency.cpp
+++ Difficulty: Medium/Next element with greater frequency/next-element-with-greater-frequency.cpp
@@ -1,33 +1,37 @@
 class Solution {
   public:
-    vector<int> findGreater(vector<int>& arr) {
+    vector<int> nextFreqGreater(vector<int>& arr) {
         // code here
+         int n = arr.size();
+         unordered_map<int,int>mp;
          
-                  unordered_map<int,int>mp;
-                  for(int i:arr) mp[i]++;
-                  int n= arr.size();
-                  vector<int>ans(n,-1);
-                  stack<int>st;
-                //   st.push(-1);
-                  for(int i=n-1;i>=0;i--){
-                           
-                           while (!st.empty() && mp[arr[i]]>=mp[st.top()]){
-                                 st.pop();
-                           }
-                           
-                                           
-                                 if (st.empty()){
-                                      ans[i] = -1;
-                                 } 
-                           else ans[i] = st.top();
-                           
-                           st.push(arr[i]);
-                           
-                           
-                  }
-                  
-                  return ans;
-           
+         for(auto i:arr){
+             
+              mp[i]++;
+              
+         } 
+         
+         vector<int>ans(n,-1);
+         stack<pair<int,int>>st;
+         st.push({arr[n-1],mp[arr[n-1]]});
+         for(int i=n-2;i>=0;i--){
+               
+            while(!st.empty() && st.top().second<=mp[arr[i]]){
+                  st.pop();
+            }
+            
+            if(!st.empty()){
+                
+                 ans[i]= st.top().first;
+            }
+            
+             st.push({arr[i],mp[arr[i]]});
+                
+         }
+         
+         return ans;
+         
+         
          
     }
 };

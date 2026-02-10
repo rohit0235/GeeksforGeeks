304bf4dd3cdf51cf2be338693c894330e369b44d
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2026-02-10 09:01:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Koko Eating Bananas/koko-eating-bananas.cpp
+++ Difficulty: Medium/Koko Eating Bananas/koko-eating-bananas.cpp
@@ -1,54 +1,39 @@
 class Solution {
   public:
-  
-      bool ispos(int mid ,vector<int>& arr, int k ){
-           
-           int count =0;
-           
-           for(int i:arr){
-                 
-                   count+=((i+mid-1)/mid);
-                     
-                   if (count>k) return false;
-                
-           }
-           
-           return true;
-           
-      }
-  
-  
+   
+    bool pos(vector<int>&arr, int k, int mid){
+            int total =0;
+            
+            for(int i:arr){
+                   if(total>k) return false;
+                   if(i<mid) total++;
+                   else total+=((i+mid-1)/mid);
+            }
+            
+            return total<=k;
+    }
+    
     int kokoEat(vector<int>& arr, int k) {
-        // Code here
-        
-             int maxelement = *max_element(arr.begin(),arr.end());
-             
-             int l=1;
-             int r=maxelement;
-              int ans =0;
-             while (l<=r){
-                   
-                   int mid =(l+r)/2;
-                   
-                   
-                   if (ispos(mid,arr,k)){
-                        ans=mid;
-                        r=mid-1;
-                   }
-                   else{
-                        l=mid+1;
-                   }
-                   
-                   
-             }
-             
-             
-             return ans;
+          int n =arr.size();
+          
+          int l =1;
+          int r = *max_element(arr.begin(),arr.end());
+          int ans = -1;
+          while (l<=r){
               
-             
-             
-             
-        
-        
+                 int mid =  l + (r-l)/2;
+                 
+                 if(pos(arr,k, mid)){
+                       ans = mid;
+                       r=mid-1;
+                 }
+                 else{
+                      l = mid+1;
+                 }
+                 
+          }
+          
+          return ans;
+           
     }
 };
